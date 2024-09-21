a2b689e5aecac3aeb852dceee066ba7de8e51d62
Rayzen7 <nugrahasetio6@gmail.com>
2024-09-21 15:01:17 +0700

    update

--- src/components/data/Education.js
+++ src/components/data/Education.js
@@ -2,12 +2,12 @@ export const educationTimeline = [
   {
     year: '2019',
     title: 'Graduated from Elementary School',
-    description: 'Completed my primary education at SDN 01 Jakarta.'
+    description: 'Completed my primary education at SDN Kebon Kosong 01 Jakarta.'
   },
   {
     year: '2023',
     title: 'Graduated from Middle School',
-    description: 'Graduated from SMPN 02 Jakarta with a focus on Mathematics and Science.'
+    description: 'Graduated from SMPN 59 Jakarta with a focus on Mathematics and Science.'
   },
   {
     year: '2024 - now',
